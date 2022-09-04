c0abf017c0e926a22522b3f63224b50a4ef95005
HazAger <HazAger777@yandex.ru>
2022-09-04 18:14:35 +0300

    ended functional CRUD

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,3 +1,4 @@
+import Post from "../models/Post.js";
 import PostModel from "../models/Post.js";
 
 export const getAllPosts = async (req, res) => {
@@ -28,6 +29,103 @@ export const createPost = async (req, res) => {
   } catch (err) {
     res.status(500).json({
       message: "Не удалось создать статью",
+    });
+  }
+};
+
+export const getOnePost = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    PostModel.findOneAndUpdate(
+      {
+        _id: postId,
+      },
+      {
+        $inc: { viewsCount: 1 },
+      },
+      {
+        returnDocument: "after",
+      },
+      (err, doc) => {
+        if (err) {
+          return res.status(500).json({
+            message: "Не удалось получить статью",
+          });
+        }
+
+        if (!doc) {
+          return res.status(404).json({
+            message: "Статья не найдена",
+          });
+        }
+
+        res.json(doc);
+      }
+    );
+  } catch (err) {
+    res.status(500).json({
+      message: "Не удалось получить статью",
+    });
+  }
+};
+
+export const deletePost = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    PostModel.findByIdAndDelete(
+      {
+        _id: postId,
+      },
+      (err, doc) => {
+        if (err) {
+          return res.status(500).json({
+            message: "Не удалось удалить статью",
+          });
+        }
+
+        if (!doc) {
+          return res.status(404).json({
+            message: "Статья не найдена",
+          });
+        }
+
+        res.json({
+          success: true,
+        });
+      }
+    );
+  } catch (err) {
+    res.status(500).json({
+      message: "Не удалось удалить статью",
+    });
+  }
+};
+
+export const updatePost = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    await PostModel.updateOne(
+      {
+        _id: postId,
+      },
+      {
+        title: req.body.title,
+        text: req.body.text,
+        imageUrl: req.body.imageUrl,
+        tags: req.body.tags,
+        user: req.userId,
+      }
+    );
+
+    res.json({
+      success: true,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Не удалось обновить статью",
     });
   }
 };
--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
+import multer from "multer";
 
-import { register, login, profile } from "./controllers/UserController.js";
-import { createPost, getAllPosts } from "./controllers/PostController.js";
+import { UserController, PostController } from "./controllers/index.js";
+import { checkAuth, handleValidationErrors } from "./middlewares/index.js";
+
 import {
   loginValidation,
   registerValidation,
   postCreateValidation,
 } from "./validations.js";
-
-import checkAuth from "./middlewares/checkAuth.js";
 
 mongoose
   .connect(
@@ -20,16 +20,59 @@ mongoose
 
 const app = express();
 
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
 const PORT = process.env.PORT || 4444;
 
 app.use(express.json());
+app.use("/uploads", express.static("uploads"));
 
-app.post("/auth/login", loginValidation, login);
-app.post("/auth/register", registerValidation, register);
-app.get("/auth/profile", checkAuth, profile);
+app.post(
+  "/auth/login",
+  loginValidation,
+  handleValidationErrors,
+  UserController.login
+);
+app.post(
+  "/auth/register",
+  registerValidation,
+  handleValidationErrors,
+  UserController.register
+);
+app.get("/auth/profile", checkAuth, UserController.profile);
 
-app.post("/posts", checkAuth, postCreateValidation, createPost);
-app.get("/posts", getAllPosts);
+app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  });
+});
+
+app.post(
+  "/posts",
+  checkAuth,
+  postCreateValidation,
+  handleValidationErrors,
+  PostController.createPost
+);
+app.delete("/posts/:id", checkAuth, PostController.deletePost);
+app.patch(
+  "/posts/:id",
+  checkAuth,
+  postCreateValidation,
+  handleValidationErrors,
+  PostController.updatePost
+);
+app.get("/posts", PostController.getAllPosts);
+app.get("/posts/:id", PostController.getOnePost);
 
 app.listen(PORT, (err) => {
   if (err) {
